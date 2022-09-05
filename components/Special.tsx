9f2a093d1023614443d0d383317a9248353c57a3
Dark-Lover <person.zen15@gmail.com>
2022-09-05 23:57:24 +0100

    fix mobile trend slider bug

--- components/Special.tsx
+++ components/Special.tsx
@@ -9,8 +9,8 @@ import { settings } from "./../utils/SliderSettings";
 
 function Special({ specialData }: { specialData: Product[] }) {
   return (
-    <section className="h-screen  mb-16   sm:h-80">
-      <div className="h-full grid grid-rows-2  sm:grid-rows-1 sm:grid-cols-4">
+    <section className="h-screen mb-16 sm:h-80">
+      <div className="h-full  grid grid-rows-2  sm:grid-rows-1 sm:grid-cols-4">
         <div className=" relative flex flex-col justify-center items-center gap-10 bg-black sm:col-span-1">
           <h3 className="text-white font-semibold">New Trend Edition</h3>
           <div className="relative before:content-['_'] before:w-24 before:h-24 before:border-2 before:absolute before:overflow-hidden ">
@@ -26,7 +26,7 @@ function Special({ specialData }: { specialData: Product[] }) {
             Explore All <BsArrowRight />
           </button>
         </div>
-        <div className=" sm:inline-block sm:px-6 sm:col-span-3  gap-2 sm:my-auto">
+        <div className=" w-96 mt-4 mx-auto sm:w-full sm:inline-block sm:px-6 sm:col-span-3  gap-2 sm:my-auto">
           {/* <div className="relative flex flex-col sm:flex-row sm:gap-2 "> */}
           <Slider {...settings}>
             {specialData.map((prod) => {
