1be023f96d018f3f8357d9d0e8d9d70cd0641864
Dark-Lover <person.zen15@gmail.com>
2022-09-23 23:58:07 +0100

    add basic form with formik for checkout

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -1,16 +1,70 @@
+import { Formik, FormikErrors, FormikValues } from "formik";
+interface IInitVal {
+  firstName: string;
+  lastName: string;
+}
+
 function Checkout() {
+  const initialVal: IInitVal = { firstName: "", lastName: "" };
+  console.log("Checkout Render now");
   return (
-    <div className="flex flex-col p-4 bg-bgs-green sm:flex-1">
-      <h2 className="font-semibold mb-4 bg-red-400">Checkout</h2>
-      <form>
-        <label className="text-xs">First Name</label>
-        <input
-          type="text"
-          name="firstname"
-          placeholder="Enter First Name"
-          className="ml-4"
-        />
-      </form>
+    <div className="flex flex-col p-4  sm:flex-1">
+      <h2 className="font-semibold mb-4 ">Checkout</h2>
+      <Formik
+        initialValues={initialVal}
+        validate={(values) => {
+          const errors: FormikErrors<FormikValues> = {};
+          if (!values.firstName) {
+            errors.firstName = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.firstName)
+          ) {
+            errors.firstName = "Invalid firstName address";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstname">First Name</label>
+            <input
+              type="firstName"
+              name="firstName"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.firstName}
+            />
+            {errors.firstName && touched.firstName && errors.firstName}
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              type="lastName"
+              name="lastName"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.lastName}
+            />
+            {errors.lastName && touched.lastName && errors.lastName}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
